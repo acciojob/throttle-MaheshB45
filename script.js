e4d96f74cdcc8c4deb42dd13d214b3f32431e785
Mahesh Rajendra Bahir <107680212+MaheshB45@users.noreply.github.com>
2024-01-23 18:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 //your JS code here. If required.
 function throttle(callback, delay) {
+  let lastCallTime = 0;
+  let scheduledCallback;
+  let lastArgs;
 
+  const throttled = function (...args) {
+    const currentTime = Date.now();
+
+    if (currentTime - lastCallTime >= delay) {
+      // If enough time has passed, call the callback immediately
+      callback.apply(this, args);
+      lastCallTime = currentTime;
+    } else {
+      // Schedule the callback to be called after the remaining delay
+      clearTimeout(scheduledCallback);
+      lastArgs = args;
+      scheduledCallback = setTimeout(() => {
+        callback.apply(this, lastArgs);
+        lastCallTime = Date.now();
+      }, delay - (currentTime - lastCallTime));
+    }
+  };
+
+  // Add a cancel method to the throttled function
+  throttled.cancel = function () {
+    clearTimeout(scheduledCallback);
+  };
+
+  return throttled;
 }
 
 module.exports = throttle;
